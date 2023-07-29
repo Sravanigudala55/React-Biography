03371a45beace81e829346adc91a0caf5c3c7dde
sravanigudala <137883533+Sravanigudala55@users.noreply.github.com>
2023-07-29 11:40:54 +0530

    Add files via upload

---
+++ design.js
@@ -0,0 +1,25 @@
+ReactDOM.render(
+    <div>
+        <img src="https://www.chaipakodi.com/wp-content/uploads/2020/04/sundar-pichai.jpg" height="350px" width="400px"/>
+        
+        <h2>Personal Information</h2>
+        <p>Pichai Sundararajan (born June 10, 1972[3][4][5]), better known as Sundar Pichai (/ˈsʊndɑːr pɪˈtʃaɪ/), is an Indian-American business executive.[6] He is the chief executive officer (CEO) of Alphabet Inc. and its subsidiary Google.[7]
+
+         Pichai began his career as a materials engineer. Following a short stint at the management consulting firm McKinsey & Co., Pichai joined Google in 2004,[8] where he led the product management and innovation efforts for a suite of Google's client software products, including Google Chrome and ChromeOS, as well as being largely responsible for Google Drive. In addition, he went on to oversee the development of other applications such as Gmail and Google Maps. In 2010, Pichai also announced the open-sourcing of the new video codec VP8 by Google and introduced the new video format, WebM. The Chromebook was released in 2012. In 2013, Pichai added Android to the list of Google products that he oversaw.
+
+         Pichai was selected to become the next CEO of Google on August 10, 2015, after previously being appointed Product Chief by CEO Larry Page. On October 24, 2015, he stepped into the new position at the completion of the formation of Alphabet Inc., the new holding company for</p>
+        <h2>Personal Life</h2>
+        <p>Pichai is married to Anjali Pichai and has two children.[55] Anjali is originally from Kota, India. Both came close while studying together in IIT, Kharagpur.[56] His recreational interests include cricket and football.[57][58] Pichai is an adherent of Hinduism.[59]References
+        </p>
+        <h2>Career and Education</h2>
+        <p>Pichai worked in engineering and product management at Applied Materials and in management consulting at McKinsey & Company.[24] Pichai joined Google in 2004, where he led the product management and innovation efforts for a suite of Google's client software products, including Google Chrome[25] and ChromeOS, as well as being largely responsible for Google Drive. He went on to oversee the development of other applications such as Gmail and Google Maps.[26][27] On November 19, 2009, Pichai gave a demonstration of ChromeOS; the Chromebook was released for trial and testing in 2011, and released to the public in 2012.[28] On May 20, 2010, he announced the open-sourcing of the new video codec VP8 by Google and introduced the new video format, WebM.[29]On March 13, 2013, Pichai added Android to the list of Google products that he oversaw. Android was formerly managed by Andy Rubin.[30] He was a director of Jive Software from April 2011 to July 30, 2013.[31][32][33] Pichai was selected to become the next CEO of Google on August 10, 2015[34] after previously being appointed Product Chief by CEO, Larry Page. On October 24, 2015, he stepped into the new position at the completion of the formation</p>
+        
+
+            
+        
+        
+       
+        
+        
+    </div>
+    ,document.getElementById("content"));
